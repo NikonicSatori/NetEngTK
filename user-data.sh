3202752ed2569da5b48d254ccb2a70b44a11a3cf
Muto <140279133+NikonicSatori@users.noreply.github.com>
2025-04-06 11:22:41 -0400

    Update user-data.sh
    updated home dir to root since ec2 user data runs as root not ec2-user

--- user-data.sh
+++ user-data.sh
@@ -2,7 +2,7 @@
 # User Data. Specify during EC2 instance creation. This code will run once on initial boot.
 
 echo "Create NetEngTK directory"
-mkdir ~/NetEngTK && cd ~/NetEngTK
+mkdir /home/ec2-user/NetEngTK && cd /home/ec2-user/NetEngTK
 
 echo "Fetch init.sh"
 curl -O https://raw.githubusercontent.com/NikonicSatori/NetEngTK/refs/heads/main/init.sh
