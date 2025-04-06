dd93f08759ffc24167326e433e0863908d337020
Muto <140279133+NikonicSatori@users.noreply.github.com>
2025-04-06 15:12:23 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 exec > >(tee -a /var/log/init.log) 2>&1
-FILENAME=$("$0")
+FILENAME=$(basename "$0")
 echo "$FILENAME: init.sh: SCRIPT START"
 
-echo > "$FILENAME: Update system and install tools"
-
 # Update system and install tools
-sudo yum update -y
-sudo yum install -y yum-utils curl git
+echo > "$FILENAME: Update system and install tools"
+timeout 300 yum update -y || echo "$FILENAME: yum update timed out"
+timeout 300 yum install -y yum-utils curl git || echo "$FILENAME: yum install yum-utils curl git timed out"
 
 # Install Docker
 echo "$FILENAME: Install Docker"
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+timeout 300 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo || echo "$FILENAME: repo add timed out"
+timeout 300 sudo yum install -y docker-ce docker-ce-cli containerd.io || echo "install docker-ce docker-ce-cli containerd.io failed"
 
 #Enable and start Docker
 echo "$FILENAME: Enable and start Docker"
 sudo systemctl enable docker
 sudo systemctl start docker
 
+echo "starting docker... sleeping 10 seconds"
+sleep 10
+
 # Install Docker Compose
 echo "$FILENAME: Install Docker Compose"
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
-    -o /usr/local/bin/docker-compose
+timeout 300 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose || \
+    echo "$FILENAME: curl docker-compose timed out"
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Confirm installation
@@ -43,6 +46,6 @@ echo '{ "iptables": false }' | sudo tee /etc/docker/daemon.json
 echo "$FILENAME: Restart docker"
 sudo systemctl restart docker
 
-echo "$FILENAME: docker compose up -d"
+echo "$FILENAME: Next, user should run docker compose up -d"
 
 echo "$FILENAME: init.sh: SCRIPT FINISH"
