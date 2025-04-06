958cab37f24167e384add2f02972e469c586f3c1
Muto <140279133+NikonicSatori@users.noreply.github.com>
2025-04-06 12:03:14 -0400

    Update init.sh updated logging

--- init.sh
+++ init.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
+exec > >(tee -a /var/log/init.log) 2>&1
 
-echo "Update system and install tools"
+FILENAME=$("$0")
+
+echo > "$FILENAME: Update system and install tools"
 
 # Update system and install tools
 sudo yum update -y
 sudo yum install -y yum-utils curl git
 
 # Install Docker
-echo "Install Docker"
+echo "$FILENAME: Install Docker"
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 #Enable and start Docker
-echo "Enable and start Docker"
+echo "$FILENAME: Enable and start Docker"
 sudo systemctl enable docker
 sudo systemctl start docker
 
 # Install Docker Compose
-echo "Install Docker Compose"
+echo "$FILENAME: Install Docker Compose"
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
     -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Confirm installation
-echo "Confirm installation"
+echo "$FILENAME: Confirm installation"
 docker --version
 docker-compose --version
 
@@ -33,11 +36,11 @@ docker-compose --version
 
 # OR: Upload docker-compose.yml manually if not using Git yet
 
-echo "Docker and Compose installed."
+echo "$FILENAME: Docker and Compose installed."
 
-echo "Disable iptables in /etc/docker/daemon.json"
+echo "$FILENAME: Disable iptables in /etc/docker/daemon.json"
 echo '{ "iptables": false }' | sudo tee /etc/docker/daemon.json
-echo "Restart docker"
+echo "$FILENAME: Restart docker"
 sudo systemctl restart docker
 
-echo "docker compose up -d"
+echo "$FILENAME: docker compose up -d"
