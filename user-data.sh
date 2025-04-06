c1b1ecb5149a88765b66537279a01fa1a79bc4f3
Muto <140279133+NikonicSatori@users.noreply.github.com>
2025-04-06 12:07:53 -0400

    Update user-data.sh updated logging

--- user-data.sh
+++ user-data.sh
@@ -2,6 +2,8 @@
 # User Data. Specify during EC2 instance creation. This code will run once on initial boot.
 exec > >(tee -a /var/log/init.log) 2>&1
 FILENAME=$("$0")
+echo "$FILENAME: Logging to /var/log/init.log"
+echo "$FILENAME: user-data.sh: SCRIPT START"
 
 echo "$FILENAME: Create NetEngTK directory"
 mkdir /home/ec2-user/NetEngTK && cd /home/ec2-user/NetEngTK
@@ -13,4 +15,4 @@ chmod +x init.sh
 echo "$FILENAME: Run init.sh"
 ./init.sh
 
-echo "$FILENAME: user-data.sh completed!"
+echo "$FILENAME: user-data.sh: SCRIPT FINISH"
