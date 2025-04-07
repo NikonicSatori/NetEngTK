94a1aa9fb7b840b61b4db64d1d2792bf6f3d9711
Muto <140279133+NikonicSatori@users.noreply.github.com>
2025-04-06 20:42:54 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ echo "$FILENAME: init.sh: SCRIPT START"
 # Update system and install tools
 echo "$FILENAME: Update system and install tools"
 timeout 300 yum update -y || echo "$FILENAME: yum update timed out"
-timeout 300 yum install -y yum-utils curl git bind-utils || echo "$FILENAME: yum install tools timed out"
+timeout 300 yum install -y yum-utils curl git bind-utils ftp || echo "$FILENAME: yum install tools timed out"
 
 # Install Docker
 echo "$FILENAME: Install Docker"
